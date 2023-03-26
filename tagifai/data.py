2ef49214270794dad37e977aa7fbafa81af5db0e
jay-khade <jaykhade2023@gmail.com>
2023-03-27 00:16:25 +0530

    Data preprocessing

---
+++ tagifai/data.py
@@ -0,0 +1,62 @@
+# --------------------- Importing Libraries ---------------------------
+
+from nltk.stem import PorterStemmer
+import re
+import pandas as pd
+from collections import Counter
+
+from config import config
+
+
+# ---------------------------------- Preprocessing Techniques ------------------------
+
+# clean text
+def clean_text(text, lower=True, stem=False, stopwords=config.STOPWORDS) -> str:
+    """Clean row text"""
+
+    # making input text lower
+    if lower:
+        text = text.lower()
+
+    # remove stopwords
+    if len(stopwords):
+        pattern = re.compile(r'\b(' + r"|".join(stopwords) + r")\b\s*")
+        text = pattern.sub('', text)
+
+    # Spacing and filters
+    text = re.sub(
+        r"([!\"'#$%&()*\+,-./:;<=>?@\\\[\]^_`{|}~])", r" \1 ", text)  # add spacing between objects to be filtered
+    text = re.sub("[^A-Za-z0-9]+", " ", text)  # remove non-alphanumeric chars
+    text = re.sub(" +", " ", text)  # remove multiple spaces
+    text = text.strip()  # strip white space at the ends
+
+    # Remove links
+    text = re.sub(r"http\S+", "", text)
+
+    # stemming
+    if stem:
+        stemmer = PorterStemmer()
+        text = " ".join([stemmer.stem(word, to_lowercase=lower) for word in text.split(" ")])
+
+    return text
+
+
+# out of scope labels
+def replace_oss_labels(df, labels, label_col, oss_label="other") -> pd.DataFrame:
+    """Replace out of scope labels"""
+
+    oss_tags = [item for item in df[label_col].unique() if item not in labels]
+    df[label_col] = df[label_col].apply(lambda x: oss_label if x in oss_tags else x)
+    return df
+
+
+# minority labels
+def replace_minority_labels(df, label_col, min_freq, new_label="other"):
+    """Replace Minority labels with another labels"""
+
+    labels = Counter(df[label_col].values)
+    labels_above_freq = Counter(label for label in labels.elements() if (labels[label] >= min_freq))
+    df[label_col] = df[label_col].apply(lambda label: label if label in labels_above_freq else None)
+    df[label_col] = df[label_col].fillna(new_label)
+
+    return df
