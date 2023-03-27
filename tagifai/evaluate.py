bbbf99ee347d737a5f6426915c78a74441007a5a
jay-khade <jaykhade2023@gmail.com>
2023-03-27 16:47:15 +0530

    Model Training correction short text output

--- tagifai/evaluate.py
+++ tagifai/evaluate.py
@@ -1,16 +1,18 @@
 # --------------------------------- Importing Libraries --------------------------
 
+from typing import Dict, List
+
 import numpy as np
+import pandas as pd
 from sklearn.metrics import precision_recall_fscore_support
-from snorkel.slicing import PandasSFApplier
-from snorkel.slicing import slicing_function
+from snorkel.slicing import PandasSFApplier, slicing_function
 
 
 # ------------------------------ Different evaluate functions for different situations ------------------------
 
 @slicing_function()
 def nlp_cnn(x):
-    """NLP projects that uses convolution."""
+    """NLP Projects that use convolution."""
     nlp_projects = "natural-language-processing" in x.tag
     convolution_projects = "CNN" in x.text or "convolution" in x.text
     return nlp_projects and convolution_projects
@@ -18,45 +20,59 @@ def nlp_cnn(x):
 
 @slicing_function()
 def short_text(x):
-    """Projects with short title and descriptions."""
+    """Projects with short titles and descriptions."""
     return len(x.text.split()) < 8  # less than 8 words
 
 
-def get_slice_metrics(y_true, y_pred, slices):
-    """Generate metrics for slices of data."""
-
+def get_slice_metrics(y_true: np.ndarray, y_pred: np.ndarray, slices: np.recarray) -> Dict:
+    """Generate metrics for slices of data.
+    Args:
+        y_true (np.ndarray): true labels.
+        y_pred (np.ndarray): predicted labels.
+        slices (np.recarray): generated slices.
+    Returns:
+        Dict: slice metrics.
+    """
     metrics = {}
     for slice_name in slices.dtype.names:
         mask = slices[slice_name].astype(bool)
-
         if sum(mask):
-            slice_metrics = precision_recall_fscore_support(y_true[mask], y_pred[mask], average="micro")
-
+            slice_metrics = precision_recall_fscore_support(
+                y_true[mask], y_pred[mask], average="micro"
+            )
             metrics[slice_name] = {}
             metrics[slice_name]["precision"] = slice_metrics[0]
             metrics[slice_name]["recall"] = slice_metrics[1]
             metrics[slice_name]["f1"] = slice_metrics[2]
             metrics[slice_name]["num_samples"] = len(y_true[mask])
 
-            return metrics
+    return metrics
 
 
-def get_metrics(y_true, y_pred, classes, df=None):
-    """Performance metrics using ground truths and predictions"""
-
-    # performance
+def get_metrics(
+        y_true: np.ndarray, y_pred: np.ndarray, classes: List, df: pd.DataFrame = None
+) -> Dict:
+    """Performance metrics using ground truths and predictions.
+    Args:
+        y_true (np.ndarray): true labels.
+        y_pred (np.ndarray): predicted labels.
+        classes (List): list of class labels.
+        df (pd.DataFrame, optional): dataframe to generate slice metrics on. Defaults to None.
+    Returns:
+        Dict: performance metrics.
+    """
+    # Performance
     metrics = {"overall": {}, "class": {}}
 
-    # overall metrics
+    # Overall metrics
     overall_metrics = precision_recall_fscore_support(y_true, y_pred, average="weighted")
     metrics["overall"]["precision"] = overall_metrics[0]
     metrics["overall"]["recall"] = overall_metrics[1]
     metrics["overall"]["f1"] = overall_metrics[2]
     metrics["overall"]["num_samples"] = np.float64(len(y_true))
 
-    # per-class metrics
+    # Per-class metrics
     class_metrics = precision_recall_fscore_support(y_true, y_pred, average=None)
-
     for i, _class in enumerate(classes):
         metrics["class"][_class] = {
             "precision": class_metrics[0][i],
@@ -65,9 +81,9 @@ def get_metrics(y_true, y_pred, classes, df=None):
             "num_samples": np.float64(class_metrics[3][i]),
         }
 
-    # slice metrics
+    # Slice metrics
     if df is not None:
         slices = PandasSFApplier([nlp_cnn, short_text]).apply(df)
-        metrics["slices"] = get_slice_metrics(
-            y_true=y_true, y_pred=y_pred, slices=slices)
+        metrics["slices"] = get_slice_metrics(y_true=y_true, y_pred=y_pred, slices=slices)
+
     return metrics
